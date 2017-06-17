3b5deaa1c8d0c9d81beca264986e42e89b0c2175
grbatinic.bogdan <grbatinic.bogdan@gmail.com>
2017-06-17 16:32:43 +0300

    Finish up people module, add readme file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Modular JS testing/playground
+
+This is an implementation built from [LearnCode.academy](https://www.youtube.com/user/learncodeacademy) tutorial about modular JavaScript. I adapted it a bit according to my tastes:
+- Dropped jQuery support
+- Added gulp for build process
+
+## Usage
+
+Install dependencies: `npm install`
+Build modules: `gulp`
+Watch module changes `gulp watch`
+
--- js/modules/module.people.js
+++ js/modules/module.people.js
@@ -9,6 +9,42 @@ var people = (function(){
         _form = document.getElementById('personForm');
         _template = document.getElementById('people-template').textContent;
         _peopleList = document.getElementById('people');
+    }    
+
+    function _render() {
+        _peopleList.innerHTML = Mustache.render(_template, {people: _people});
+        _restartRemoveHandlers();
+    }
+
+    function _addPersonHandler() {
+        _form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            var input = document.getElementsByTagName('input')[0]
+            var personName = input.value;
+            if (personName) {
+                addPerson(personName);
+                input.value = '';
+            }
+        });
+    }
+
+    function _removePersonHandler(event) {        
+        var parentListItem = event.target.parentElement;
+        var name = event.target.previousElementSibling.textContent;        
+        if (name) {
+            removePerson(name);
+        }
+    }
+
+    function _restartRemoveHandlers() {
+        var deleteBtns = document.querySelectorAll('.del');        
+        
+        if (deleteBtns) {            
+            deleteBtns.forEach(function(deleteBtn) {
+                deleteBtn.removeEventListener('click', _removePersonHandler);
+                deleteBtn.addEventListener('click', _removePersonHandler);
+            });
+        }        
     }
 
     function addPerson(person) {
@@ -19,29 +55,17 @@ var people = (function(){
     function removePerson(person) {
         if (_people.indexOf(person) > -1) {
             _people.splice(
-                _people.indexOf(person, 1)
-            )
+                _people.indexOf(person),
+                1
+            );
         }
 
         _render();
     }
 
-    function _render() {
-        _peopleList.innerHTML = Mustache.render(_template, {people: _people});
-    }
-
-
-
     function init() {
-
         _cacheDom();
-
-        /*
-        _form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            console.log(event);
-        });
-        */
+        _addPersonHandler();        
     }
 
     return {
